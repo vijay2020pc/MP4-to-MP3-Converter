e4bc2af8a9a06c5557d4f7a918fca4c10759383e
Vijay gupta <61613472+vijay2020pc@users.noreply.github.com>
2021-07-30 23:22:54 +0530

    Update mp4tomp3.py

--- mp4tomp3.py
+++ mp4tomp3.py
@@ -1,14 +1,24 @@
-from tkinter import Tk, Button
+from tkinter import Image, Tk, Button
+from tkinter import *
 import moviepy.editor as mp
 import os
 import re
 from tkinter.filedialog import askdirectory
 from tkinter import messagebox
+from tkinter import PhotoImage
+from PIL import Image,ImageTk
 
 root = Tk()
+#p1=PhotoImage("C:\\Users\\Vijay Gupta\\Pictures\\Camera Roll\\favpng_movie-logo.png")
+#root.iconphoto(False,p1)
 root.title('Video Mp4 to Mp3 Audio converter')
-root.configure(background="light green")
-root.minsize(width=500,height=100)
+image1=Image.open("C:\\Users\\Vijay Gupta\\Pictures\\Camera Roll\\wp4676621-4k-pc-wallpapers.jpg")
+filename=ImageTk.PhotoImage(image1)
+background_label = Label(root, image=filename)
+background_label.image=filename
+background_label.place(x=0, y=0)
+
+root.minsize(width=500,height=200)
 root.resizable(width=False,height=False)
 
 def open_mp4_folder():
@@ -32,12 +42,12 @@ def convert():
 
 
 mp4_btn = Button(root,text="Choose Mp4 Folder",width=25, command=open_mp4_folder,bg="blue",fg="white")
-mp4_btn.grid(row=1,column=0,padx = 15, pady =15)
+mp4_btn.grid(row=1,column=0,padx = 50, pady =50)
 
 mp3_btn = Button(root,text="Choose Mp3 Folder",width=25, command=open_mp3_folder,bg="brown",fg="white")
 mp3_btn.grid(row=1,column=1)
 
-convert_btn = Button(root,text="Convert Mp4 to Mp3",width=25, command=convert,bg="black",fg="white")
+convert_btn = Button(root,text="Convert Mp4 to Mp3",width=25, command=convert,bg="yellow",fg="black")
 convert_btn.grid(row=2,column=0)
 
 exit_btn = Button(root,text="Exit Application",width=25, command=root.destroy,bg="purple",fg="white")
